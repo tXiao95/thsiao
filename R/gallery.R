16c4756c8ed82ff66a266dbefdcb0eb60ae014b5
Thomas Hsiao <tXiao95@gmail.com>
2020-03-06 14:20:55 -0500

    Chebyshev spectral differentiation matrix

--- R/gallery.R
+++ R/gallery.R
@@ -15,8 +15,8 @@
 #' @export
 binomial_matrix <- function(n){
   L <- abs(pascal(n, 1))
-  U <- L[n:1, n:1]
-  D <- diag((-2)^(0:(n - 1)))
+  U <- L[n:1,n:1]
+  D <- diag((-2)^(0:(n-1)))
 
   return(L %*% D %*% U)
 }
@@ -26,7 +26,8 @@ binomial_matrix <- function(n){
 #' @name cauchy_matrix
 #' @title Create Cauchy matrix
 #'
-#' @description Arguments \code{x} and \code{y} are vectors of length \code{n}. \code{C[i,j] = 1 / (x[i] + y[j])}
+#' @description Arguments \code{x} and \code{y} are vectors of length \code{n}.
+#'   \code{C[i,j] = 1 / (x[i] + y[j])}
 #'
 #' @param x vector of length n
 #' @param y vector of length n
@@ -45,7 +46,63 @@ cauchy_matrix <- function(x,y=NULL){
     stop("cauchy:ParamLengthMismatch")
   }
 
-  return(1 / (matrix(x, nrow = n, ncol = n) + matrix(y, nrow = n,ncol = n, byrow = T)))
+  return(1 / (matrix(x, nrow = n, ncol = n) + matrix(y, nrow = n, ncol = n, byrow = T)))
+}
+
+# Chebspec matrix ---------------------------------------------------------
+
+#' @name chebspec
+#' @title Create Chebyshev spectral differentiation matrix
+#'
+#' @description Chebyshev spectral differentiation matrix of order \code{n}. \code{k} determines
+#'   the character of the output matrix. For either form, the eigenvector matrix is ill-conditioned.
+#'
+#' @param n order of the matrix.
+#' @param k \code{k=0} is the default, no boundary conditions. The matrix is similar to a Jordan block
+#'   of size \code{n} with eigenvalue 0. If \code{k=1}, the matrix is nonsingular
+#'   and well-conditioned, and its eigenvalues have negative real parts.
+#'
+#' @export
+chebspec <- function(n, k = NULL){
+  if(is.null(k)){
+    k <- 0
+  }
+  if(k == 1){
+    n <- n + 1
+  }
+  if(!(k %in% 0:1)){
+    stop("k must be 0 or 1")
+  }
+
+  n <- n - 1
+  C <- matrix(0, nrow = n + 1, ncol = n + 1)
+
+  x <- cos(matrix(0:n, ncol = 1) * (pi/n))
+  d <- matrix(1, nrow = n + 1, ncol = 1)
+  d[1] <- 2; d[n + 1] <- 2
+
+  X <- matrix(x, nrow = n + 1, ncol = n + 1) - matrix(x, nrow = n + 1, ncol = n + 1, byrow = T)
+  C <- (d %*% t(matrix(1, nrow = n + 1, ncol = 1) / d)) / (X + diag(nrow(C)))
+
+  # Now fix diagonal and signs
+  C[1,1] <- (2*n^2 + 1) / 6
+  for(i in 2:(n + 1)){
+    if((i %% 2) == 0){
+      C[,i] <- -C[,i]
+      C[i,] <- -C[i,]
+    }
+    if(i < (n+1)){
+      C[i,i] <- -x[i] / (2 * (1 - x[i]^2))
+    } else{
+      C[n + 1,n + 1] <- -C[1,1]
+    }
+  }
+
+  if(k == 1){
+    C <- C[2:(n + 1),2:(n + 1)]
+  }
+
+  return(C)
 }
 
 # Sparse Diagonal Matrix --------------------------------------------------
--- tests/testthat/test-gallery.R
+++ tests/testthat/test-gallery.R
@@ -5,9 +5,33 @@ test_that("Binomial matrix", {
   for(n in 2:30){
     A <- binomial_matrix(n)
     expect_equal(norm(A %*% A - 2^(n-1) * diag(n)), 0)
+
+    # B is involutory
     B <- A*2^((1-n)/2)
     expect_equal(norm(B %*% B - diag(n)), 0)
   }
+})
+
+
+# Chebspec matrix ---------------------------------------------------------
+
+test_that("Chebspec matrix", {
+  C <- chebspec(4)
+  # C is nilpotent, k=0
+  expect_equal(norm(C %*% C %*% C %*% C), 0)
+  # C has nullspace dim 1
+  sigmas <- svd(C)$d
+  smallest <- sigmas[length(sigmas)]
+  next_smallest <- sigmas[length(sigmas)-1]
+  expect_equal(smallest, 0)
+  expect_true(next_smallest > sqrt(.Machine$double.eps))
+  # and the null vector is all ones
+  expect_equal(norm(C %*% c(1,1,1,1)), 0)
+
+  # Eigenvalues have all real parts when k=1
+  C <- chebspec(4, 1)
+  eigs <- eigen(C)$values
+  expect_true(all(Re(eigs) < 0))
 })
 
 # Tridiagonal matrix ------------------------------------------------------
