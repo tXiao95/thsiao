19c18e68cd361d9c1af0d290fd5575307275ac10
Thomas Hsiao <tXiao95@gmail.com>
2020-04-01 21:14:32 -0400

    Code and documentation for cycol function

--- R/gallery.R
+++ R/gallery.R
@@ -267,6 +267,28 @@ compar <- function(B, k = 0){
   return(A)
 }
 
+# condex ------------------------------------------------------------------
+
+# cycol: Matrix whose columns repeat cyclically-------------------------------------
+
+#' @name cycol
+#' @title Create matrix \code{A} whose columns repeat cyclically
+#'
+#' @description Returns an \code{n}-by-\code{n} matrix with cyclically repeating columns
+#'   where one cycle consists of the columns defined by \code{randn(n,k)}. Thus, the
+#'   rank of matrix \code{A} cannot exceed \code{k}, and \code{k} must be scalar.
+#'
+#' @param n number of columns of matrix
+#' @param k upper limit of rank
+#' @param m number of rows of matrix
+#'
+#' @export
+cycol <- function(n, k, m=NULL){
+  if(is.null(m)){m <- n}
+  nums <- rnorm(m*k)
+  return(suppressWarnings(matrix(nums, nrow=m, ncol=n)))
+}
+
 # spdiags: Sparse Diagonal Matrix --------------------------------------------------
 
 #' @name spdiags
