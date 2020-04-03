af8e37553d4f8c07ca1c9227e0e08c90c910fcad
Thomas Hsiao <tXiao95@gmail.com>
2020-04-02 23:24:39 -0400

    test and function for creating Dorr matrix

--- R/gallery.R
+++ R/gallery.R
@@ -289,6 +289,42 @@ cycol <- function(n, k, m=NULL){
   return(suppressWarnings(matrix(nums, nrow=m, ncol=n)))
 }
 
+# dorr: Dorr matrix -------------------------------------------------------
+
+#' @name dorr
+#' @title Create Dorr matrix
+#'
+#' @description Returns a \code{n}-by-\code{n} row diagonally dominant,
+#'   tridiagonal matrix that is ill-conditioned for small nonnegative values
+#'   of \code{theta}. The default value of \code{theta} is 0.01.
+#'
+#' @param n order of matrix
+#' @param theta determines conditionality. Ill-conditioned when theta is nonnegative.
+#'
+#' @export
+dorr <- function(n, theta=0.01){
+  h <- 1 / (n + 1)
+  m <- floor( (n + 1) / 2)
+  term <- theta / h^2
+
+  c <- rep(0, n); e <- c; d <- c
+
+  i <- 1:m
+    c[i] <- -term * rep(1, m)
+    e[i] <- c[i] - (0.5 - i * h) / h
+    d[i] <- -(c[i] + e[i])
+
+  i <- (m+1):n
+    e[i] <- -term * rep(1, n-m)
+    c[i] <- e[i] + (0.5 - i * h) / h
+    d[i] <- -(c[i] + e[i])
+
+  c <- c[2:n]
+  e <- e[1:(n-1)]
+
+  return(tridiag(n, c, d, e))
+}
+
 # spdiags: Sparse Diagonal Matrix --------------------------------------------------
 
 #' @name spdiags
--- tests/testthat/test-gallery.R
+++ tests/testthat/test-gallery.R
@@ -120,6 +120,24 @@ test_that("clement matrix", {
   expect_true(isSymmetric(B))
 })
 
+# dorr: Dorr matrix -------------------------------------------------------
+
+test_that("Dorr matrix is correct", {
+  # theta 0.01
+  Atrue <- matrix(c(1.32, -1.16, 0,
+                    -.16, .32, -.16,
+                    0, -1.16, 1.32), nrow=3, byrow=T)
+  Atest <- as.matrix(dorr(3))
+  expect_equal(norm(Atest - Atrue), 0)
+
+  # negative theta
+  Btrue <- matrix(c(-31, 15, 0,
+                    16, -32, 16,
+                    0, 15, -31), nrow=3, byrow=T)
+  Btest <- as.matrix(dorr(3, -1))
+  expect_equal(norm(Btest - Btrue), 0)
+})
+
 # tridiag: Sparse tridiagonal matrix ------------------------------------------------------
 
 test_that("Tridiagonal matrix eigenvalues", {
