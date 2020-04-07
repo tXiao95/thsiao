f8ad530605e475adb76379b8c106374f608988bd
Thomas Hsiao <tXiao95@gmail.com>
2020-04-06 21:52:39 -0400

    anti-hadamard, forsythe, and jordan block code

--- R/gallery.R
+++ R/gallery.R
@@ -325,6 +325,111 @@ dorr <- function(n, theta=0.01){
   return(tridiag(n, c, d, e))
 }
 
+# dramadah: Matrix of zeros and ones --------------------------------------
+
+#' @name dramadah
+#' @title Create anti-Hadamard matrix \code{A}
+#'
+#' @description Returns a \code{n}-by-\code{n} nonsingular matrix of 0's and 1's.
+#'   With large determinant or inverse. If \code{k=1}, \code{A} is Toeplitz and \code{abs(det(A))=1}.
+#'   If \code{k=2}, \code{A} is upper triangular and Toeplitz. If \code{k=3}, \code{A} has maximal
+#'   determinant among (0,1) lower Hessenberg matrices. Also is Toeplitz.
+#'
+#'   Also known as an anti-Hadamard matrix.
+#'
+#' @param n order of matrix
+#' @param k decides type of matrix returned.
+#'
+#' @export
+dramadah <- function(n, k=1){
+  if(k == 1){ # Toeplitz
+    c <- rep(1, n)
+    for(i in seq(2, n, 4)){
+      m <- min(1, n-i)
+      c[i:(i+m)] <- 0
+    }
+    r <- rep(0, n)
+    r[1:4] <- c(1, 1, 0, 1)
+    if(n < 4){
+      r <- r[1:n]
+    }
+    A <- Toeplitz(c, r)
+  } else if(k == 2){ # Upper triangular and Toeplitz
+    c <- rep(0, n)
+    c[1] <- 1
+    r <- rep(1, n)
+    r[seq(3, n, 2)] <- 0
+    A <- Toeplitz(c, r)
+  } else if(k == 3){ # Lower Hessenberg
+    c <- rep(1, n)
+    c[seq(2, n, 2)] <- 0
+    A <- Toeplitz(c, c(1, 1, rep(0, n-2)))
+  }
+  return(A)
+}
+
+# fiedler: Fiedler Symmetric matrix ------------------------------------------------
+
+#' @name fiedler
+#' @title Create Fiedler matrix
+#'
+#' @description Fiedler matrix that has a dominant positive eigenvalue and all others are negative
+#'
+#' @param c N-vector. If \code{c} is a scalar, then returns fiedler(1:c)
+#'
+#' @return a symmetric dense matrix A with a dominant positive eigenvalue and all others are negative.
+#'
+#' @export
+fiedler <- function(c){
+  if (!is.vector(c)){
+    stop("'c' is not a vector")
+  }
+  if(length(c) == 1){
+    c <- 1:c
+  }
+  n <- length(c)
+  A <- matrix(raw(), n, n)
+  A <- matrix(data = abs(c[col(A)] - c[row(A)]), nrow = n, ncol = n)
+
+  return(A)
+}
+
+# forsythe: Perturbed Jordan block ----------------------------------------------------------------
+
+#' @name forsythe
+#' @title Create Forsythe matrix or perturbed Jordan block
+#'
+#' @description Returns a \code{n}-by-\code{n} matrix equal to the Jordan block with
+#'   eigenvalue \code{lambda}, except that \code{A[n,1]=alpha}.
+#'
+#' @param n order of matrix
+#' @param alpha value of perturbation at \code{A[n,1]}
+#' @param lambda eigenvalue of Jordan block
+#'
+#' @export
+forsythe <- function(n, alpha=.Machine$double.eps, lambda = 0){
+  A <- jordbloc(n, lambda)
+  A[n, 1] <- alpha
+  return(A)
+}
+
+# jordbloc: Jordan block matrix -------------------------------------------
+#'
+#' @name jordbloc
+#' @title Create Jordan block matrix
+#'
+#' @description Returns a \code{n}-by-\code{n} JOrdan block with eigenvalue
+#'   \code{lambda}. The default is 1.
+#'
+#' @param n order of matrix
+#' @param lambda eigenvalue of Jordan block
+#'
+#' @export
+jordbloc <- function(n, lambda = 1){
+  J <- lambda * diag(n) + Diag(rep(1, n-1), 1)
+  return(J)
+}
+
 # spdiags: Sparse Diagonal Matrix --------------------------------------------------
 
 #' @name spdiags
@@ -408,32 +513,6 @@ tridiag <- function(n, x=NULL, y=NULL, z=NULL){
   n <- length(y)
 
   return(spdiags(matrix(c(x, 0, y, 0, z), nrow = n), -1:1, n, n))
-}
-
-# fiedler: Fiedler Symmetric matrix ------------------------------------------------
-
-#' @name fiedler
-#' @title Create Fiedler matrix
-#'
-#' @description Fiedler matrix that has a dominant positive eigenvalue and all others are negative
-#'
-#' @param c N-vector. If \code{c} is a scalar, then returns fiedler(1:c)
-#'
-#' @return a symmetric dense matrix A with a dominant positive eigenvalue and all others are negative.
-#'
-#' @export
-fiedler <- function(c){
-  if (!is.vector(c)){
-    stop("'c' is not a vector")
-  }
-  if(length(c) == 1){
-    c <- 1:c
-  }
-  n <- length(c)
-  A <- matrix(raw(), n, n)
-  A <- matrix(data = abs(c[col(A)] - c[row(A)]), nrow = n, ncol = n)
-
-  return(A)
 }
 
 # grcar: Toeplitz matrix with sensitive eigenvalues ------------------------------
